afb9e9426a34c307917fa3060ef82242688e33ae
Soumya Mandi <soumyamandi@gmail.com>
2012-10-17 11:56:04 +0530

    all signal handlers added. now cannot stop the program :P

--- sig.c
+++ sig.c
@@ -2,13 +2,34 @@
 #include <signal.h>
 #include <unistd.h>
 
+
+// int sigaction(int signum, const struct sigaction *act,
+ //                    struct sigaction *oldact);
+
+void handler( int signum ) {
+	if( signum == SIGINT ) {
+		printf("SIGINT caught\n") ;
+	}
+	if( signum == SIGTERM ) {
+
+		printf("SIGTERM caught\n") ;
+	}
+	if( signum == SIGTSTP ) {
+
+		printf("SIGTSTP caught\n") ;
+	}
+}
+
 int main( int argc,char** argv ) {
+	signal( SIGINT,handler ) ;
+	signal( SIGTERM,handler ) ;
+	signal( SIGTSTP,handler ) ;
 	int clock_tick = 0 ;
 	while( 1 ) {
 		printf("tick %d...\n",clock_tick ) ;
 		clock_tick += 10 ;
 		sleep( 10 ) ;
-		
+
 
 
 	}
